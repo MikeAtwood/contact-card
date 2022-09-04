ebe0df2f2de37a12de6ada430fcb344c87e0e1f7
Deadpool1 <atwood169@yahoo.com>
2022-09-04 13:51:12 -0500

    Add to staging lesson 2

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,42 +1,17 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack")
+const path = require("path");
 
 module.exports = {
-    entry: "./src/js/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            title: 'Webpack Plugin'
-        })
-    ],
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['@babel/preset-env', { targets: "defaults" }]
-                        ]
-                    }
-                }
-            }
-        ]
-    }
-    
-}
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  ],
+  mode: "development"
+};
